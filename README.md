723ffc11ed610fac6a56435f71573e2ac1123a74
Nelson Roque <nelsonroquejr@gmail.com>
2023-05-31 10:28:39 -0400

    up

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # contextlab_datascience_bootcamp
  
 ## Dataset links
-Brazil Hourly
+Brazil Hourly - https://www.kaggle.com/datasets/PROPPG-PPG/hourly-weather-surface-brazil-southeast-region
 IMDB Movies
 Oasis Dementia
---
+++ oasis_dementia/load.R
@@ -0,0 +1,4 @@
+library(readr)
+oasis_cross_sectional <- read_csv("oasis_dementia/data/oasis_cross-sectional.csv")
+oasis_longitudinal <- read_csv("oasis_dementia/data/oasis_longitudinal.csv")
+View(oasis_cross_sectional)
